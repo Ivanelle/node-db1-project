87f4269f28d41220b225480b695fb7c03ce50024
Ivanelle Hunt <ivanellep@gmail.com>
2024-01-15 13:29:46 -0800

    delete working

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -22,7 +22,6 @@ router.post('/',
  md.checkAccountPayload,
  md.checkAccountNameUnique,
  async (req, res, next) => {
-  // DO YOUR MAGIC
   try {
     const newAccount = await Account.create(req.body)
     res.status(201).json(newAccount)
@@ -45,10 +44,10 @@ router.put('/:id',
   }
 });
 
-router.delete('/:id', md.checkAccountId, (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
   try {
-    res.json('delete accounts')
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
   } catch (err) {
     next(err)
   }
