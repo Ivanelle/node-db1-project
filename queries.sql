b6cf44124d9f82125570f588dba73020b7306758
Ivanelle Hunt <ivanellep@gmail.com>
2024-01-15 11:58:31 -0800

    sqlite queries working, and error handling and endpoints wired!

--- queries.sql
+++ queries.sql
@@ -1,16 +1,35 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+    SELECT * 
+    FROM Customers 
+    WHERE PostalCode='1010';
 
 -- Find the phone number for the supplier with the id 11
+    SELECT phone 
+    FROM suppliers 
+    WHERE supplierId=11;
 
 -- List first 10 orders placed, sorted descending by the order date
-
+    SELECT * 
+    FROM orders 
+    ORDER BY orderdate desc
+    LIMIT 10;
 -- Find all customers that live in London, Madrid, or Brazil
+    SELECT * 
+    FROM customers 
+    WHERE city IN ('london', 'madrid') 
+    OR country IN ('brazil');
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+INSERT INTO customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE Customers 
+SET postalcode = 11122
+WHERE customername = 'Bilbo Baggins';
+
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
