e88b7bb8e19999bf1a1517d509c8c3c2d451c56e
Ivanelle Hunt <ivanellep@gmail.com>
2024-01-15 13:00:54 -0800

    checkAccountPayload is working

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,16 +1,30 @@
 const Account = require('../accounts/accounts-model')
 
-exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
-  console.log('check account PM')
+exports.checkAccountPayload = async (req, res, next) => {
+  const { name, budget } = req.body
+  if (name === undefined || budget === undefined) {
+    res.status(400).json({
+      message: 'name and budget are required'
+    })
+  } else if ( name.trim().length < 3 || name.trim().length > 100) {
+    res.status(400).json({
+      message: 'name of account must be between 3 and 100'
+    })
+  } else if (typeof budget !== typeof 1 && !parseInt(budget)) {
+    res.status(400).json({
+      message: 'budget of account must be a number'
+    })
+  }  else if (budget < 0 || budget > 1000000) {
+    res.status(400).json({
+      message: 'budget of account is too large or too small'
+    })
+  }
+
   next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('check account NU')
+  
   next()
 }
 
